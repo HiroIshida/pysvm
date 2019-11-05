e698b900ec717c142360742ffa06383c840a87b5
HiroIshida <spitfire.docomo@gmail.com>
2019-11-05 13:53:22 +0900

    add show scatter

--- svm.py
+++ svm.py
@@ -2,7 +2,9 @@ import math
 import numpy as np
 import numpy.linalg as la
 import numpy.random as rn
+import matplotlib.pyplot as plt
 import cvxopt
+import time
 
 class rbf_kernel:
 
@@ -19,6 +21,15 @@ class SVM:
         self.n_dim = X.shape[1]
         self.kern = kern
         self.C = C
+
+    def show(self):
+        idx_positive = np.where(svm.Y > 0)[0]
+        idx_negative = np.where(svm.Y < 0)[0]
+        fig, ax = plt.subplots() 
+        ax.scatter(self.X[idx_positive, 0], self.X[idx_positive, 1], c = "r")
+        ax.scatter(self.X[idx_negative, 0], self.X[idx_negative, 1], c = "b")
+
+
 
     def solve_qp(self):
         P = np.zeros((self.n_train, self.n_train))
@@ -44,10 +55,25 @@ class SVM:
         sol = cvxopt.solvers.qp(P, q, G, h, A, b)
         return sol
 
+def gen_dataset(N = 10):
+    X = rn.randn(N, 3)
+    logical = np.array([(X[i, 1] - X[i, 0] * 2 < 0) for i in range(N)])
+    Y = (-1 + logical * 2)
+    return X, Y
+
+
 if __name__=='__main__':
     kern = rbf_kernel(1.0)
-    N= 2
-    X = rn.randn(N, 3)
-    Y = (-1 + (rn.randn(N) > 0)*2)
+    X, Y = gen_dataset(30)
+    #X = rn.randn(N, 3)
+    #Y = (-1 + (rn.randn(N) > 0)*2)
     svm = SVM(X, Y, kern)
+
+    ts = time.time()
     sol = svm.solve_qp()
+    te = time.time()
+    print te - ts
+
+    svm.show()
+
+
