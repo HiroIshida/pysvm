5ea6f03bac58f937371ae2e0efa2da833d9cb837
HiroIshida <spitfire.docomo@gmail.com>
2019-11-06 10:14:00 +0900

    change package name to avoid confliction

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name='svm',
+    name='pysvm',
     version='0.0.1',
     description='Sample package for Python-Guide.org',
     license=license,
